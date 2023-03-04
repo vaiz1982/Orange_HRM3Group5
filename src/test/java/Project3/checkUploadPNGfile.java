f2992556e0a0d56f2e12c74c3efce5cecf300352
vaiz1982 <alexeyvoyts@gmail.com>
2023-03-04 16:04:27 -0500

    my first commit from scrath how are you doing by the way??

---
+++ src/test/java/Project3/checkUploadPNGfile.java
@@ -0,0 +1,7 @@
+package Project3;
+
+public class checkUploadPNGfile {
+    public static void main(String[] args) {
+        System.out.println("Do you able to see my code? Just check it out please! ");
+    }
+}
